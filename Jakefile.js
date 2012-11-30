ff7e2583acc04d1c706693076f303d0227b22322
James Shore <jshore@jamesshore.com>
2012-11-29 18:11:55 -0800

    Installed Testacular, Mocha, and Expect.js

--- Jakefile.js
+++ Jakefile.js
@@ -3,6 +3,10 @@
 (function () {
 	"use strict";
 
+	var REQUIRED_BROWSERS = [
+		"IE 8.0"
+	];
+
 	var lint = require("./build/lint_runner.js");
 	var nodeunit = require("./build/nodeunit_runner.js");
 
@@ -10,6 +14,11 @@
 	task("default", ["lint", "test"], function() {
 		console.log("\n\nOK");
 	});
+
+	desc("Start Testacular server for testing");
+	task("testacular", function() {
+		testacular(["start", "build/testacular.conf.js"], "Could not start Testacular server", complete);
+	}, {async: true});
 
 	desc("Lint everything");
 	task("lint", [], function () {
@@ -23,7 +32,43 @@
 			if (failures) fail("Tests failed");
 			complete();
 		});
-	}, {async:true});
+	}, {async: true});
+
+	function testacular(args, errorMessage, callback) {
+		args.unshift("node_modules/testacular/bin/testacular");
+		sh("node", args, errorMessage, callback);
+	}
+
+	function sh(command, args, errorMessage, callback) {
+		console.log("> " + command + " " + args.join(" "));
+
+		// Not using jake.createExec as it adds extra line-feeds into output as of v0.3.7
+		var child = require("child_process").spawn(command, args, { stdio: "pipe" });
+
+		// redirect stdout
+		var stdout = "";
+		child.stdout.setEncoding("utf8");
+		child.stdout.on("data", function(chunk) {
+			stdout += chunk;
+			process.stdout.write(chunk);
+		});
+
+		// redirect stderr
+		var stderr = "";
+		child.stderr.setEncoding("utf8");
+		child.stderr.on("data", function(chunk) {
+			stderr += chunk;
+			process.stderr.write(chunk);
+		});
+
+		// handle process exit
+		child.on("exit", function(exitCode) {
+			if (exitCode !== 0) fail(errorMessage);
+		});
+		child.on("close", function() {      // 'close' event can happen after 'exit' event
+			callback(stdout, stderr);
+		});
+	}
 
 	function nodeFilesToTest() {
 		var testFiles = new jake.FileList();
