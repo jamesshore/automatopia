d0b70f2258a1d9d2cf784585b0412de5a1201282
James Shore <jshore@jamesshore.com>
2012-11-29 18:28:55 -0800

    Got client test code working

--- Jakefile.js
+++ Jakefile.js
@@ -3,8 +3,8 @@
 (function () {
 	"use strict";
 
-	var REQUIRED_BROWSERS = [
-		"IE 8.0"
+	var TESTED_BROWSERS = [
+		"Safari 6.0"
 	];
 
 	var lint = require("./build/lint_runner.js");
@@ -28,12 +28,44 @@
 	});
 
 	desc("Test everything");
-	task("test", [], function() {
+	task("test", ["testClient"], function() {
 		nodeunit.runTests(nodeFilesToTest(), function(failures) {
 			if (failures) fail("Tests failed");
 			complete();
 		});
 	}, {async: true});
+
+	desc("Test client code");
+	task("testClient", function() {
+		var config = {};
+
+		var output = "";
+		var oldStdout = process.stdout.write;
+		process.stdout.write = function(data) {
+			output += data;
+			oldStdout.apply(this, arguments);
+		};
+
+		require("testacular/lib/runner").run(config, function(exitCode) {
+			process.stdout.write = oldStdout;
+
+			if (exitCode) fail("Client tests failed (to start server, run 'jake testacular')");
+			var browserMissing = false;
+			TESTED_BROWSERS.forEach(function(browser) {
+				browserMissing = checkIfBrowserTested(browser, output) || browserMissing;
+			});
+			if (browserMissing && !process.env.loose) fail("Did not test all supported browsers (use 'loose=true' to suppress error)");
+			if (output.indexOf("TOTAL: 0 SUCCESS") !== -1) fail("Client tests did not run!");
+
+			complete();
+		});
+	}, {async: true});
+
+	function checkIfBrowserTested(browser, output) {
+		var missing = output.indexOf(browser + ": Executed") === -1;
+		if (missing) console.log(browser + " was not tested!");
+		return missing;
+	}
 
 	function nodeFilesToTest() {
 		var testFiles = new jake.FileList();
--- build/testacular.conf.js
+++ build/testacular.conf.js
@@ -11,7 +11,7 @@ files = [
   MOCHA,
   MOCHA_ADAPTER,
   'node_modules/expect.js/expect.js',
-  'src/**/*.js'
+  'src/client/**/*.js'
 ];
 
 
