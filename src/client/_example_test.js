5897c61ed63f4b5bf50c481dd87569cf62bdcc00
James Shore <jshore@jamesshore.com>
2013-03-07 21:09:05 -0800

    Disabled Mocha global leak detection to prevent error on Firefox 19

--- src/client/_example_test.js
+++ src/client/_example_test.js
@@ -1,7 +1,9 @@
 // Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
-/*global describe, it, expect, example, beforeEach */
+/*global describe, it, expect, example, beforeEach, mocha */
 (function() {
 	"use strict";
+
+	mocha.setup({ignoreLeaks: true});
 
 	describe("Text field validator", function() {
 
