375854b3fcbfaceeac2b72c011941876b8e0d24e
James Shore <jshore@jamesshore.com>
2015-01-21 22:08:16 -0800

    Installed 'karma-commonjs' module for modularity with Karma

--- build/config/karma.conf.js
+++ build/config/karma.conf.js
@@ -11,7 +11,7 @@
 
 
 	    // frameworks to use
-	    frameworks: ['mocha', 'expect'],
+	    frameworks: ['mocha', 'expect', 'commonjs'],
 
 
 	    // list of files / patterns to load in the browser
@@ -24,6 +24,13 @@
 	    exclude: [
 
 	    ],
+
+
+	    // preprocess matching files before serving them to the browser
+	    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+	    preprocessors: {
+		    'src/client/**/*.js': ['commonjs'],
+	    },
 
 
 	    // test results reporter to use
