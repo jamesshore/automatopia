4c7f08c17bca8428f2cecc0b83dcbb6a401f0c3b
James Shore <jshore@jamesshore.com>
2016-09-21 10:14:59 -0700

    Updated tested_browsers.js to more modern examples

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -3,14 +3,16 @@
 	"use strict";
 
 	// Uncomment and modify the following list to cause the build to fail unless these browsers are tested.
+	// The strings must match the values reported by Karma.
 
 	module.exports = [
-		//"IE 8.0.0 (Windows XP)",
-		//"IE 9.0.0 (Windows 7)",
-		//"Firefox 23.0.0 (Mac OS X 10.8)",
-		//"Chrome 29.0.1547 (Mac OS X 10.8.5)",
-		//"Safari 6.0.5 (Mac OS X 10.8.5)",
-		//"Mobile Safari 6.0.0 (iOS 6.1)"
+		// "IE 11.0.0 (Windows 7 0.0.0)",
+		// "Edge 13.10586.0 (Windows 10 0.0.0)",
+		// "Firefox 48.0.0 (Mac OS X 10.11.0)",
+		// "Chrome 53.0.2785 (Mac OS X 10.11.6)",
+		// "Safari 9.1.3 (Mac OS X 10.11.6)",
+		// "Mobile Safari 10.0.0 (iOS 10.0.0)",
+		// "Chrome Mobile 44.0.2403 (Android 6.0.0)"
 	];
 
 }());
