4559a664122c13ce86c25dfc7d704bca94335a65
James Shore <jshore@jamesshore.com>
2024-09-23 17:40:26 -0700

    Add example source code

---
+++ src/_hello_test.js
@@ -0,0 +1,13 @@
+// Copyright Titanium I.T. LLC.
+"use strict";
+
+// A real codebase would use a npm module for the test suite here.
+const { suite, assert } = require("../_build/node_modules/tests");
+
+module.exports = suite(({ it }) => {
+
+	it("runs tests", () => {
+		// assert.todo();
+	});
+
+});
